1714b0440fa2c1f46ae5a2fb826e6ddf057d7508
Omkar Kadam <kadam.omkar@outlook.com>
2024-01-02 01:32:46 +0530

    Added parameterized tests for email validation

--- user_registration/src/test/java/user_registration/FirstNameValidatorTest.java
+++ user_registration/src/test/java/user_registration/FirstNameValidatorTest.java
@@ -68,4 +68,38 @@ public class FirstNameValidatorTest {
     void testEmailIsValid(){
         assertTrue(EmailValidator.isValid("abc-100@yahoo.com"));
     }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"abc@yahoo.com",
+                            "abc-100@yahoo.com",
+                            "abc.100@yahoo.com",
+                            "abc111@abc.com",
+                            "abc-100@abc.net",
+                            "abc.100@abc.com.au",
+                            "abc@1.com",
+                            "abc@gmail.com.com",
+                            "abc+100@gmail.com"})
+    void testEmailIsValidHappy(String str) {
+
+        assertTrue(EmailValidator.isValid(str));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"abc",
+                "abc@.com.my",
+                "abc123@gmail.a",
+                "abc123@.com",
+                "abc123@.com.com",
+                ".abc@abc.com",
+                "abc()*@gmail.com",
+                "abc@%*.com",
+                "abc..2002@gmail.com",
+                "abc.@gmail.com",
+                "abc@abc@gmail.com",
+                "abc@gmail.com.1a",
+                "abc@gmail.com.aa.au"})
+    void testEmailIsValidSad(String str) {
+
+        assertFalse(EmailValidator.isValid(str));
+    }
 }
